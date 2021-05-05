ea3b3e7760504620a977f99778393e7698c47392
MarkusSollLavaGecastetWerden <78972070+MarkusSollLavaGecastetWerden@users.noreply.github.com>
2021-05-05 10:21:21 +0200

    Add files via upload

---
+++ Fotostory/index.js
@@ -0,0 +1,6 @@
+var id = setInterval(diashow, 5000);
+
+function diashow() {
+    document.getElementsByClassName("images").src = "pictures/Bild3.jpg";
+    document.getElementsByClassName("details").innerHTML = "Moin moin meine meisters!";
+}
