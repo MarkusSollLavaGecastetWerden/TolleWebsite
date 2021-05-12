7b55882dbbed4d8e9173019989cf11ce2460964f
MarkusSollLavaGecastetWerden <9118@htl.rennweg.at>
2021-05-12 10:47:09 +0200

    moin

--- Fotostory/details.js
+++ Fotostory/details.js
@@ -1,5 +1,11 @@
-window.addEventListener("load", getEXIF);
+window.addEventListener("load", getExif);
 
-function getEXIF() {
-    alert("sjflsjdflk");
+function getExif() {
+    var img1 = document.getElementById("pic1");
+    var txt = EXIF.getData(img1, function() {
+        var make = EXIF.getTag(this, "Make");
+        var model = EXIF.getTag(this, "Model");
+        var makeAndModel = document.getElementById("txt");
+            makeAndModel.innerHTML = `${make} ${model}`;
+    });
 }
