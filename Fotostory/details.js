b53afa3ce3ab525968509bc7303385bff0697aeb
CodingGott <markus.wizany@gmail.com>
2021-05-12 10:52:33 +0200

    moin

--- Fotostory/details.js
+++ Fotostory/details.js
@@ -1,5 +1,11 @@
 window.addEventListener("load", getEXIF);
 
+document.getElementById("pic1").addEventListener("onclick", changePic1);
+
 function getEXIF() {
     alert("sjflsjdflk");
+}
+
+function changePic1() {
+    window.alert();
 }
