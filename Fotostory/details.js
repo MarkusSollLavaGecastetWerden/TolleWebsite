a44bd9323f3cfde3a2f58fc6aae2e380ac9fd793
MarkusSollLavaGecastetWerden <9118@htl.rennweg.at>
2021-05-12 10:58:21 +0200

    moin

--- Fotostory/details.js
+++ Fotostory/details.js
@@ -2,7 +2,7 @@ window.addEventListener("load", getExif);
 
 function getExif() {
     var img1 = document.getElementById("pic1");
-    var txt = EXIF.getData(img1, function() {
+    EXIF.getData(img1, function() {
         var make = EXIF.getTag(this, "Make");
         var model = EXIF.getTag(this, "Model");
         var makeAndModel = document.getElementById("txt");
