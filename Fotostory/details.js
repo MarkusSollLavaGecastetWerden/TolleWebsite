647dd5f40af70fead6252053db4e465437aad7c3
MarkusSollLavaGecastetWerden <9118@htl.rennweg.at>
2021-05-25 21:05:27 +0200

    moin

--- Fotostory/details.js
+++ Fotostory/details.js
@@ -1,42 +1,68 @@
 window.addEventListener("load", load);
-window.addEventListener("load", getExif);
+/*window.addEventListener("load", getExif);*/
 
 let planung = {
     "becher": {
         "zeit": 30,
         "iso": 100,
+        "blende": "groß",
     },
     "donau": {
         "zeit": 30,
         "iso": 100,
+        "blende": "mittel",
     },
     "ubahn": {
         "zeit": 1,
         "iso": 100,
+        "blende": "klein",
     },
     "prater": {
         "zeit": 30,
         "iso": 100,
+        "blende": "mittel",
     },
     "gloriette": {
         "zeit": 30,
         "iso": 100,
+        "blende": "mittel",
     },
     "innenstadt": {
         "zeit": 2,
         "iso": 100,
+        "blende": "mittel",
     }
 };
+
+let wirklichKeit = [];
+
+let fotos = ["becher", "becher", "donau", "ubahn", "prater", "gloriette", "gloriette", "innenstadt"];
 
 function load() {
     for (let i = 1; i <= 8; i++) {
         document.getElementById("cont" + i).addEventListener("mouseover", () => {
-            document.getElementById("cont" + i).innerHTML = "";
+            document.getElementById("cont" + i).innerHTML = generateText(i);
         });
         document.getElementById("cont" + i).addEventListener("mouseout", () => {
             document.getElementById("cont" + i).innerHTML = '<img src="pictures/Bild' + i + '.jpg" id="pic' + i + '">';
         });
     }
+}
+
+function generateText(index) {
+    let text = "";
+    let data = planung[fotos[index]];
+    text += "<h1>Planung</h1>"
+    text += "Belichtungszeit: 1/" + data.zeit + "<br>";
+    text += "ISO: " + data.iso + "<br>";
+    text += "Blende: " + data.blende + "<br>";
+
+    data = wirklichKeit[index];
+    text += "<h1>Realität</h1>"
+    text += "Belichtungszeit: 1/" + data.zeit + "<br>";
+    text += "ISO: " + data.iso + "<br>";
+    text += "Blende: " + data.blende + "<br>";
+    return text;
 }
 
 function getExif() {
